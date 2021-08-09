2bcebccd81625a8a5d76615a77956a8bb63c03d6
Drew Dandrea <d.dandrea00@gmail.com>
2021-08-09 10:00:16 -0400

    divide homepage into smaller components, finish styling MenuItem component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory';
+import './Home.scss';
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Directory />
+    </div>
+  );
+}
+
+export default Home;
