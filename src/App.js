07bbf351adcfeb3192c179cde7e8ce8013d4e730
Drew Dandrea <d.dandrea00@gmail.com>
2021-08-09 14:27:48 -0400

    update MenuItem to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Home from './pages/Home/Home';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
